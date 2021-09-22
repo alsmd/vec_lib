714edd29ca7790531a6f26f5182ef37cd4a97113
alsmd <dangerveneta@gmail.com>
2021-09-22 11:35:43 -0300

    normalize and magnitude

--- vec.h
+++ vec.h
@@ -6,21 +6,25 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 08:25:49 by user42            #+#    #+#             */
-/*   Updated: 2021/09/22 10:59:26 by user42           ###   ########.fr       */
+/*   Updated: 2021/09/22 11:33:21 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VEC_H
 #define VEC_H
 #include <stdlib.h>
+#include <math.h>
+
 typedef struct s_vec
 {
 	float	value[2];
+	float	magnitude;
 	void	(*sum)(struct s_vec *, struct s_vec *);
 	void	(*sub)(struct s_vec *, struct s_vec *);
 	void	(*mult)(struct s_vec *, struct s_vec *);
 	void	(*div)(struct s_vec *, struct s_vec *);
 	void	(*scale)(struct s_vec *, float);
+	void	(*normalize)(struct s_vec *);
 } t_vec;
 
 
@@ -29,6 +33,8 @@ void	sub(t_vec *v1, t_vec *v2);
 void	mult(t_vec *v1, t_vec *v2);
 void	_div(t_vec *v1, t_vec *v2);
 void	scale(t_vec *v1, float scale);
+void	normalize(t_vec *v);
+
 t_vec	*new_vec(float x, float y);
 
 #endif
--- vector.c
+++ vector.c
@@ -6,11 +6,12 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 08:02:52 by user42            #+#    #+#             */
-/*   Updated: 2021/09/22 10:57:12 by user42           ###   ########.fr       */
+/*   Updated: 2021/09/22 11:33:40 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vec.h"
+static void	magnitude(t_vec *v);
 
 t_vec	*new_vec(float x, float y)
 {
@@ -24,6 +25,9 @@ t_vec	*new_vec(float x, float y)
 	vec->mult = mult;
 	vec->div = _div;
 	vec->scale = scale;
+	vec->normalize = normalize;
+	magnitude(vec);
+
 	return (vec);
 }
 
@@ -55,4 +59,15 @@ void	_div(t_vec *v1, t_vec *v2)
 {
 	v1->value[0] /=  v2->value[0];
 	v1->value[1] /=  v2->value[1];
+}
+
+static void	magnitude(t_vec *v)
+{
+	v->magnitude = sqrt((v->value[0] * v->value[0]) + (v->value[1] * v->value[1]));
+}
+
+void	normalize(t_vec *v)
+{
+	v->value[0] /= v->magnitude;
+	v->value[1] /= v->magnitude;
 }
